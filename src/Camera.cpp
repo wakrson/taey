8bfb820ce8f81ee9273b8b4251a463a232161e09
Mark Robinson <wakrson@gmail.com>
2025-12-13 16:17:53 -0500

    remove this from camera

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -9,113 +9,113 @@
 #include "FramePoint.h"
 
 Camera::Camera() {
-  this->scale_ = 1.0;
-  this->height_ = 0;
-  this->width_ = 0;
-  this->dist_coeffs_.resize(5);
-  std::fill(this->dist_coeffs_.begin(), this->dist_coeffs_.end(), 0);
-  this->fx_ = 0;
-  this->fy_ = 0;
-  this->cx_ = 0;
-  this->cy_ = 0;
-  this->pose_ = Eigen::Transform<double, 3, Eigen::Isometry>::Identity();
+  scale_ = 1.0;
+  height_ = 0;
+  width_ = 0;
+  dist_coeffs_.resize(5);
+  std::fill(dist_coeffs_.begin(), dist_coeffs_.end(), 0);
+  fx_ = 0;
+  fy_ = 0;
+  cx_ = 0;
+  cy_ = 0;
+  pose_ = Eigen::Transform<double, 3, Eigen::Isometry>::Identity();
 };
 
 Camera::Camera(const YAML::Node &config) {
-  this->scale_ = config["depth_scale"].as<float>();
-  this->height_ = config["height"].as<int>();
-  this->width_ = config["width"].as<int>();
+  scale_ = config["depth_scale"].as<float>();
+  height_ = config["height"].as<int>();
+  width_ = config["width"].as<int>();
 
-  this->fx_ = config["fx"].as<float>();
-  this->fy_ = config["fy"].as<float>();
-  this->cx_ = config["cx"].as<float>();
-  this->cy_ = config["cy"].as<float>();
+  fx_ = config["fx"].as<float>();
+  fy_ = config["fy"].as<float>();
+  cx_ = config["cx"].as<float>();
+  cy_ = config["cy"].as<float>();
 
   for (const auto &val : config["distortion"]) {
-    this->dist_coeffs_.push_back(val.as<float>());
+    dist_coeffs_.push_back(val.as<float>());
   }
-  this->pose_ = Eigen::Transform<double, 3, Eigen::Isometry>::Identity();
+  pose_ = Eigen::Transform<double, 3, Eigen::Isometry>::Identity();
 }
 
 Camera::operator gtsam::Cal3_S2() const {
-  return gtsam::Cal3_S2(this->fx(), this->fy(), 0.0, this->cx(), this->cy());
+  return gtsam::Cal3_S2(fx(), fy(), 0.0, cx(), cy());
 }
 
-int Camera::height() const { return this->height_; }
+int Camera::height() const { return height_; }
 
-int Camera::width() const { return this->width_; }
+int Camera::width() const { return width_; }
 
 cv::Mat Camera::getCameraMatrix() const {
-  return (cv::Mat_<float>(3, 3) << this->fx(), 0.0, this->cx(), 0.0, this->fy(),
-          this->cy(), 0.0, 0.0, 1.0);
+  return (cv::Mat_<float>(3, 3) << fx(), 0.0, cx(), 0.0, fy(),
+          cy(), 0.0, 0.0, 1.0);
 }
 
 cv::Mat Camera::getDistCoeffs() const {
-  return cv::Mat(this->dist_coeffs_).clone();
+  return cv::Mat(dist_coeffs_).clone();
 }
 
 Eigen::Vector3f Camera::getRvec() const {
-  Eigen::AngleAxisf aa(this->pose_.cast<float>().rotation());
+  Eigen::AngleAxisf aa(pose_.cast<float>().rotation());
   Eigen::Vector3f rvec(aa.axis()(0) * aa.angle(), aa.axis()(1) * aa.angle(),
                        aa.axis()(2) * aa.angle());
   return rvec;
 }
 
-float Camera::getScale() const { return this->scale_; }
+float Camera::getScale() const { return scale_; }
 
 Eigen::Transform<double, 3, Eigen::Isometry> Camera::pose() const {
-  return this->pose_;
+  return pose_;
 }
 
 void Camera::setPose(const Eigen::Transform<double, 3, Eigen::Isometry> &pose) {
-  this->pose_ = pose;
+  pose_ = pose;
 }
 
 void Camera::setPose(const Eigen::Matrix4f &pose) {
-  this->pose_.matrix() = pose.cast<double>();
+  pose_.matrix() = pose.cast<double>();
 }
 
 void Camera::setPose(gtsam::Pose3 &pose) {
-  this->pose_.matrix().block(0, 0, 3, 3) =
+  pose_.matrix().block(0, 0, 3, 3) =
       pose.rotation().matrix().cast<double>();
-  this->pose_.matrix().block(0, 3, 3, 1) = pose.translation().cast<double>();
+  pose_.matrix().block(0, 3, 3, 1) = pose.translation().cast<double>();
 }
 
 void Camera::setRvec(const Eigen::Vector3f &rvec) {
   float angle(rvec.norm());
   Eigen::Vector3f axis(rvec.normalized());
   Eigen::AngleAxisf aa(angle, axis);
-  this->pose_.matrix().block<3, 3>(0, 0) =
+  pose_.matrix().block<3, 3>(0, 0) =
       Eigen::Matrix3f(aa.matrix()).cast<double>();
 }
 
 void Camera::setTranslation(const Eigen::Vector3f &tvec) {
-  this->pose_.matrix().block<3, 1>(0, 3) = tvec.cast<double>();
+  pose_.matrix().block<3, 1>(0, 3) = tvec.cast<double>();
 }
 
 cv::Mat Camera::undistort(const cv::Mat &src) {
   cv::Mat dst;
-  cv::undistort(src, dst, this->getCameraMatrix(), this->getDistCoeffs());
+  cv::undistort(src, dst, getCameraMatrix(), getDistCoeffs());
   return dst.clone();
 }
 
-float Camera::fx() const { return this->fx_; }
+float Camera::fx() const { return fx_; }
 
-float Camera::fy() const { return this->fy_; }
+float Camera::fy() const { return fy_; }
 
-float Camera::cx() const { return this->cx_; }
+float Camera::cx() const { return cx_; }
 
-float Camera::cy() const { return this->cy_; }
+float Camera::cy() const { return cy_; }
 
-float Camera::k1() const { return this->dist_coeffs_[0]; }
+float Camera::k1() const { return dist_coeffs_[0]; }
 
-float Camera::k2() const { return this->dist_coeffs_[1]; }
+float Camera::k2() const { return dist_coeffs_[1]; }
 
-float Camera::p1() const { return this->dist_coeffs_[2]; }
+float Camera::p1() const { return dist_coeffs_[2]; }
 
-float Camera::p2() const { return this->dist_coeffs_[3]; }
+float Camera::p2() const { return dist_coeffs_[3]; }
 
-float Camera::k3() const { return this->dist_coeffs_[4]; }
+float Camera::k3() const { return dist_coeffs_[4]; }
 
 void Camera::projectPoints(
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud) const {
@@ -138,9 +138,9 @@ void Camera::projectPoints(
 
   // Project to camera frame
   Eigen::MatrixXf pC(3, n);
-  pC.row(0) = (xs.array() - this->cx()) * zs.array() / this->fx();
-  pC.row(1) = (ys.array() - this->cy()) * zs.array() / this->fy();
-  pC.row(2) = zs.array() / this->getScale();
+  pC.row(0) = (xs.array() - cx()) * zs.array() / fx();
+  pC.row(1) = (ys.array() - cy()) * zs.array() / fy();
+  pC.row(2) = zs.array() / getScale();
 
   // Repopulate point cloud in camera frame
   for (std::size_t i = 0; i < n; i++) {
@@ -153,7 +153,7 @@ void Camera::projectPoints(
 
 std::vector<cv::Point2f>
 Camera::projectPoints(const std::vector<cv::Point3f> &object_points) const {
-  Eigen::Transform<double, 3, Eigen::Isometry> pose = this->pose();
+  Eigen::Transform<double, 3, Eigen::Isometry> pose = pose();
   Eigen::AngleAxisd aa(pose.rotation());
   Eigen::Vector3d rvec_(aa.axis()(0) * aa.angle(), aa.axis()(1) * aa.angle(),
                         aa.axis()(2) * aa.angle());
@@ -161,8 +161,8 @@ Camera::projectPoints(const std::vector<cv::Point3f> &object_points) const {
   cv::Mat tvec = (cv::Mat_<double>(3, 1) << pose.translation()(0),
                   pose.translation()(1), pose.translation()(2));
   std::vector<cv::Point2f> image_points;
-  cv::projectPoints(object_points, rvec, tvec, this->getCameraMatrix(),
-                    this->getDistCoeffs(), image_points);
+  cv::projectPoints(object_points, rvec, tvec, getCameraMatrix(),
+                    getDistCoeffs(), image_points);
   return image_points;
 }
 
@@ -172,7 +172,7 @@ Eigen::MatrixXd Camera::projectPoints(const Eigen::MatrixXd &pW) const {
     throw std::runtime_error("Invalid shape: should be [n x 3]");
   }
   // Extract transformation (T_wc)
-  Eigen::Transform<double, 3, Eigen::Isometry> wTc = this->pose();
+  Eigen::Transform<double, 3, Eigen::Isometry> wTc = pose();
   Eigen::Matrix3d rmat = wTc.rotation().transpose();
   Eigen::Vector3d tvec = -rmat * wTc.translation();
   // Transform points into the camera using (T_wc)^-1 -> T_cw
@@ -180,14 +180,14 @@ Eigen::MatrixXd Camera::projectPoints(const Eigen::MatrixXd &pW) const {
   // Normalize camera coordinates
   // Obtain camera intrinsics
   Eigen::Matrix3d K;
-  cv::cv2eigen(this->getCameraMatrix().clone(), K);
+  cv::cv2eigen(getCameraMatrix().clone(), K);
   // Project normalized camera coordinates into image frame (pixels -> [u, v])
   Eigen::MatrixXd pI = (K * pC).colwise().hnormalized();
   return pI.transpose();
 }
 
 Eigen::MatrixXd Camera::transformPoints(const Eigen::MatrixXd &pW) const {
-  Eigen::Transform<double, 3, Eigen::Isometry> Tcw = this->pose().inverse();
+  Eigen::Transform<double, 3, Eigen::Isometry> Tcw = pose().inverse();
   Eigen::Matrix3d rmat = Tcw.rotation();
   Eigen::Vector3d tvec = Tcw.translation();
   Eigen::MatrixXd pC = (rmat * pW.transpose()).colwise() + tvec;
@@ -204,7 +204,7 @@ Camera::backProject(const std::shared_ptr<FramePoint> &frame_point,
   // Extract depth
   Eigen::VectorXd Z(1);
   Z(0) = frame_point->cameraPoint()(2);
-  return this->backProject(pI, Z, frame);
+  return backProject(pI, Z, frame);
 }
 
 Eigen::MatrixXd Camera::backProject(const Eigen::MatrixXd &pI,
@@ -215,7 +215,7 @@ Eigen::MatrixXd Camera::backProject(const Eigen::MatrixXd &pI,
   }
 
   Eigen::MatrixXd K;
-  cv::cv2eigen(this->getCameraMatrix(), K);
+  cv::cv2eigen(getCameraMatrix(), K);
 
   // Backproject pixels to the camera frame
   Eigen::MatrixXd rays = K.inverse() * (pI.transpose().colwise().homogeneous());
@@ -226,7 +226,7 @@ Eigen::MatrixXd Camera::backProject(const Eigen::MatrixXd &pI,
   // pW
   if (frame == Camera::Frame::WORLD) {
     // Twc
-    Eigen::Transform<double, 3, Eigen::Isometry> wTc = this->pose();
+    Eigen::Transform<double, 3, Eigen::Isometry> wTc = pose();
     Eigen::MatrixXd pW = (wTc.linear() * pC).colwise() + wTc.translation();
     return pW.transpose();
   }
@@ -256,7 +256,7 @@ Camera::extractORB(const cv::Mat &image, const cv::Mat &depth) {
         depth.at<float>(int(key_points[i].pt.y), int(key_points[i].pt.x)));
   }
 
-  Eigen::MatrixXd pC = this->backProject(pI, Z, Camera::Frame::CAMERA);
+  Eigen::MatrixXd pC = backProject(pI, Z, Camera::Frame::CAMERA);
   // Back-project image point (pixel) into camera frame (xyz)
   std::vector<std::shared_ptr<FramePoint>> frame_points;
   for (Eigen::Index i = 0; i < pC.rows(); i++) {
@@ -367,8 +367,8 @@ std::vector<std::shared_ptr<FramePoint>> Camera::match(
 }
 
 bool Camera::isWithinImage(const Eigen::Vector2i &pI) const {
-  return (pI(0) > 0 && pI(0) < this->width_) &&
-         (pI(1) > 0 && pI(1) < this->height_);
+  return (pI(0) > 0 && pI(0) < width_) &&
+         (pI(1) > 0 && pI(1) < height_);
 }
 
 Eigen::MatrixXd Camera::generatePixelGrid(int height, int width,
